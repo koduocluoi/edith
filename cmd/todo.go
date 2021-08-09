50dbd4e27f22c60722bec06699f98d0d5b502cf2
m00nb0w <tnlong1997@gmail.com>
2021-08-09 23:31:26 +0700

    Update todo command

--- cmd/todo.go
+++ cmd/todo.go
@@ -24,13 +24,8 @@ import (
 // todoCmd represents the todo command
 var todoCmd = &cobra.Command{
 	Use:   "todo",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A todoist command",
+	Long:  `A CLI tool for todoist`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("todo called")
 	},
